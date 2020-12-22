6782d4303b9beff93659d122a59bb3ff5b3244fa
Jonas Hecht <jonas.hecht@codecentric.de>
2020-12-22 19:02:39 +0100

    Moving to Junit5 for the most of the project, since Spring Boot 2.4.x is now based on it. But we have to include vintage-engine also for the TestContainers DockerCompose module, since without the ClassRule (which is bound to Junit 4) the test is not running, because containers aren't spun up correctly...

--- server-alice/src/test/java/de/jonashackt/RestClientCertTest.java
+++ server-alice/src/test/java/de/jonashackt/RestClientCertTest.java
@@ -1,17 +1,16 @@
 package de.jonashackt;
 
 import de.jonashackt.controller.ServerAliceController;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@RunWith(SpringRunner.class)
+
 @SpringBootTest(
 		classes = ServerAliceApplication.class,
 		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
